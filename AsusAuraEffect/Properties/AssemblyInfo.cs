40214efc996ec9e8f53f65baff05d9e9ddc0aeec
Kureha Hisame <kureha@gmail.com>
2021-07-17 15:01:15 +0900

    - Edit AssemblyInfo.cs

--- AsusAuraEffect/Properties/AssemblyInfo.cs
+++ AsusAuraEffect/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AsusAuraEffect")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © Kureha Hisame 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
